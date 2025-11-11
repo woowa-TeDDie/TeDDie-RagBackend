6cd83575951b10e94183fdfac159c0fa36891890
jyt6640 <jyt6640@gmail.com>
2025-11-11 18:46:50 +0900

    test(test_ragEndpoint): search_endpoint가 올바른 응답을 하는 테스트 로직 수정

--- test/test_ragEndpoint.py
+++ test/test_ragEndpoint.py
@@ -6,3 +6,4 @@ def test_search_endpoint_exists():
     client = TestClient(app)
     response = client.post("/api/search", json={"query": "test query"})
     assert response.status_code != 404
+    assert response.status_code == 200
